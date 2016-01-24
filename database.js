bce24bf21a986d99209b6f3a563ad7b3e49acc80
Niels Overwijn <niels.overwijn@gmail.com>
2016-01-24 22:22:48 +0000

    Added database.js skeleton

---
+++ database.js
@@ -0,0 +1,31 @@
+
+var Database = function() {
+	var scope = this;
+
+	this.checkDatabase = function() {
+
+	};
+
+	this.createDatabase = function() {
+
+	};
+
+	this.checkTable = function() {
+
+	};
+
+	this.createTable = function() {
+
+	};
+
+	this.insert = function(data) {
+
+	};
+
+	this.get = function(dateStart, dateEnd) {
+
+	};
+
+};
+
+module.exports = Database;
