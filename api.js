ebec737a7da415f6b3f542469dd6ca0032edf670
Niels Overwijn <niels.overwijn@gmail.com>
2016-01-30 09:58:59 +0100

    Updated API path to /api

--- api.js
+++ api.js
@@ -9,31 +9,31 @@ var seconds_in_day = 60*60*24;
 
 var Api = function() {
 	var app = express();
-	app.get('/', function (req, res) {
+	app.get('/api/', function (req, res) {
 		db.getLastEntry(function(row) {
 			res.json(row);
 		});
 	});
 
-	app.get('/:start_date/:end_date', function(req, res) {
+	app.get('/api/:start_date/:end_date', function(req, res) {
 		db.get(req.params.start_date, req.params.end_date, function(rows) {
 			res.json(rows);
 		});
 	});
 
-	app.get('/extremes', function(req, res) {
+	app.get('/api/extremes', function(req, res) {
 		db.extremes(null, null, function(rows) {
 			res.json(rows);
 		});
 	});
 
-	app.get('/extremes/:start_date/:end_date', function(req, res) {
+	app.get('/api/extremes/:start_date/:end_date', function(req, res) {
 		db.extremes(req.params.start_date, req.params.end_date, function(rows) {
 			res.json(rows);
 		});
 	});
 
-	app.get('/today', function(req, res) {
+	app.get('/api/today', function(req, res) {
 		var now = new Date();
 		var seconds_today = (now.getHours() * 60 * 60) + (now.getMinutes() * 60) + now.getSeconds();
 		db.get(now.getTime() - seconds_today, now.getTime(), function(rows) {
@@ -41,14 +41,14 @@ var Api = function() {
 		});
 	});
 
-	app.get('/week', function(req, res) {
+	app.get('/api/week', function(req, res) {
 		var now = new Date();
 		var seconds_today = (now.getHours() * 60 * 60) + (now.getMinutes() * 60) + now.getSeconds();
 		db.get(now.getTime() - seconds_today - (seconds_in_day * 6), now.getTime(), function(rows) {
 			res.json(rows);
 		});
 	});
-	
+
 	app.listen(3000);
 };
 
