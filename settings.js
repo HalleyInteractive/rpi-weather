a1c1dd8c9626ac46cdef5be7796c35e18b255a89
Niels Overwijn <niels.overwijn@gmail.com>
2016-02-17 07:48:57 +0000

    Added initial graph test

--- settings.js
+++ settings.js
@@ -1,7 +1,8 @@
 var settings = {
 	measure_interval: 1000,
 	database_file: 'weather.db',
-	server_port: 8080
+	server_port: 8080,
+	milliseconds_in_day: 60*60*24*1000
 };
 
 module.exports = settings;
