6cf810ef382917d0f84cb14dcd7a50ce8b4ae72d
Niels Overwijn <niels.overwijn@gmail.com>
2016-02-24 08:03:00 +0000

    Added failing gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,17 @@ gulp.task('jscs', () => {
     .pipe(jscs())
     .pipe(jscs.reporter());
 });
+
+gulp.task('jscs-fix', () => {
+  return gulp.src('*.js')
+    .pipe(jscs({ fix: true }))
+    .pipe(jscs.reporter())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('test-jscs', () => {
+  return gulp.src('*.js')
+    .pipe(jscs())
+    .pipe(jscs.reporter())
+    .pipe(jscs.reporter('fail'));
+});
