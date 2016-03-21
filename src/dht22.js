9abd08612a0a5381368c6633508f7adf861497b9
Niels Overwijn <niels.overwijn@gmail.com>
2016-03-21 08:01:59 +0000

    Added roundhalf method

--- src/dht22.js
+++ src/dht22.js
@@ -24,12 +24,23 @@
     }
 
     temperature() {
-      return readout.temperature;
+      return this.roundHalf(readout.temperature);
     }
 
     humidity() {
-      return readout.humidity;
+      return this.roundHalf(readout.humidity);
     }
+
+    /**
+    * roundHalf
+    * Rounds a given floating number to it's closes .5 value
+    * @param num {float} Floating number
+    * @return {float} Floating number rounded to closest .5 value
+    **/
+    roundHalf(num) {
+      return Math.round(num * 2) / 2;
+    }
+
   }
   module.exports = new DHT22();
 })();
