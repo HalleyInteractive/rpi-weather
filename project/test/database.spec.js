aa005299621097c7d8a15000897570974f540eba
Niels Overwijn <niels.overwijn@gmail.com>
2016-04-03 17:01:14 +0100

    Added more tests

--- project/test/database.spec.js
+++ project/test/database.spec.js
@@ -9,6 +9,21 @@
 
   describe('Database', function() {
 
+    describe('Failing getLastEntry, no entries', function() {
+      it('Should fail the getLastEntry and call reject', function(done) {
+        db.getLastEntry({
+          metric:'temperature',
+        })
+        .then(function() {
+          assert.fail('Should not complete');
+          done();
+        })
+        .catch(function() {
+          done();
+        });
+      });
+    });
+
     describe('Insert tests', function(){
       describe('Temperature', function() {
     		it('Should add yesterdays temperature', function() {
@@ -29,6 +44,19 @@
           return db.insertHumidity(settings.testValues.humidity.today);
         });
     	});
+
+      describe('Failing insert', function() {
+        it('Should fail the insert and call reject', function(done) {
+          db.insert({device:-1, metric:'temperature', date: null,})
+          .then(function() {
+            assert.fail('Should not complete');
+            done();
+          })
+          .catch(function() {
+            done();
+          });
+        });
+      });
     });
 
     describe('Get entries tests', function() {
@@ -100,6 +128,40 @@
             done(error);
           });
       	});
+      });
+
+      describe('Failing get', function() {
+        it('Should fail the get and call reject', function(done) {
+          db.get({
+            device:-1,
+            metric:'temperature',
+            dateStart: null,
+            dateEnd: null
+          })
+          .then(function() {
+            assert.fail('Should not complete');
+            done();
+          })
+          .catch(function() {
+            done();
+          });
+        });
+      });
+
+      describe('Failing getLastEntry', function() {
+        it('Should fail the getLastEntry and call reject', function(done) {
+          db.getLastEntry({
+            device:-1,
+            metric:'temperature',
+          })
+          .then(function() {
+            assert.fail('Should not complete');
+            done();
+          })
+          .catch(function() {
+            done();
+          });
+        });
       });
 
     });
