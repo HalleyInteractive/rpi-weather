c891a4cb783fc1d2f64caaae9f53fdc371d5f017
Niels Overwijn <niels.overwijn@gmail.com>
2016-04-03 17:01:24 +0100

    restored jscs tests

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,20 @@ const gulp = require('gulp');
 const jscs = require('gulp-jscs');
 
 gulp.task('jscs', () => {
-  return gulp.src('./src/*.js')
+  return gulp.src('./project/*.js')
     .pipe(jscs())
     .pipe(jscs.reporter());
 });
 
 gulp.task('jscs-fix', () => {
-  return gulp.src('./src/*.js')
+  return gulp.src('./project/*.js')
     .pipe(jscs({ fix: true }))
     .pipe(jscs.reporter())
     .pipe(gulp.dest('./'));
 });
 
 gulp.task('test-jscs', () => {
-  return gulp.src('./src/*.js')
+  return gulp.src('./project/*.js')
     .pipe(jscs())
     .pipe(jscs.reporter())
     .pipe(jscs.reporter('fail'));
